9ce5d354f094a271f10c0eca1fabeda21fd88537
Divine <divywealth1@gmail.com>
2023-10-07 23:22:53 +0100

    update BASIC_HTTP baseUrl

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -2,12 +2,10 @@ import axios from "axios";
 import store from "../store/index.js";
 
 export const BASIC_HTTP = axios.create({
-  baseURL: "https://carashop.onrender.com/",
+  baseURL: "https://carashop-be.onrender.com/",
   timeout: 10000,
 });
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJEaXZpbmUiLCJsYXN0TmFtZSI6Ik9udW9yYSIsImVtYWlsIjoiZGl2eXdlYWx0aDFAZ21haWwuY29tIiwicGhvbmVObyI6IjA5MDE4MzE3ODE3IiwicGFzc3dvcmQiOiIkMmIkMTAkTExETEJvUEpOM2EzcE5SMWVvTmt2LldPeGNMVkcwSTE3enh0cTJNNFYxZkgzVU4uTWFnd3UiLCJjcmVhdGVkQXQiOiIyMDIzLTA1LTA5VDE0OjMwOjQ0LjUyM1oiLCJ1cGRhdGVkQXQiOiIyMDIzLTA1LTA5VDE0OjMwOjQ0LjUyM1oiLCJkZWxldGVkQXQiOm51bGx9LCJpYXQiOjE2ODYwNDM1NDgsImV4cCI6MTY4NjEyOTk0OH0.eLK72lc9kfsiJBarxS-7QLDbMPEpDPq1uHT6c7Rw2mE";
 
 export let BEARER_HTTP = axios.create({
   baseURL: "https://carashop-be.onrender.com/",
